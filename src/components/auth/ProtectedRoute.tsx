225f77820fd59f8acc894e86138bbfb7a8274724
Hrishikesh Rajan <hrishikeshrajan3@gmail.com>
2023-11-28 08:45:37 +0530

    Fix: Resolve issue with Protected Wrapper Component

--- src/components/auth/ProtectedRoute.tsx
+++ src/components/auth/ProtectedRoute.tsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 import { ProtectedRouteProps } from '.';
 
 function ProtectedRoute({ isAuthenticated, authenticationPath, outlet }:ProtectedRouteProps) {
-  if (Object.keys(isAuthenticated).length > 1) {
+  if (isAuthenticated && Object.keys(isAuthenticated).length > 1) {
     return outlet;
   }
   return (
