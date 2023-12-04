0559a7ccbb181582f845269e03438cd9c8de5d3e
Hrishikesh Rajan <hrishikeshrajan3@gmail.com>
2023-12-04 10:30:43 +0530

    Refactor: Signup form updated to formik

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -1,125 +1,118 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
-import { errorParser, signup } from './apis/signup.api';
+import React from 'react';
+import {
+  ErrorMessage, Field, Form, Formik,
+} from 'formik';
+import { toFormikValidationSchema } from 'zod-formik-adapter';
+import { StatusCodes } from 'http-status-codes';
+import { signup } from './apis/signup.api';
+import { registerSchema } from './helpers/validationSchema.helper';
+import Loading from '../../utils/animations/Loading';
 
 function Signup() {
-  const [fields, setFields] = useState({
-    fullname: '',
-    email: '',
-    password: '',
-  });
-
-  const [errorStatus, setErrorStatus] = useState({
-    fullname: '',
-    email: '',
-    password: '',
-    error: false,
-  });
-
-  const [successMsg, setSuccessMsg] = useState<string>('');
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFields({ ...fields, [event.target.name]: event.target.value });
-  };
-
-  const onSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    const user = {
-      fullname: fields.fullname || undefined,
-      email: fields.email || undefined,
-      password: fields.password || undefined,
-    };
-
-    signup(user).then((response: any) => {
-      if (!response.success) {
-        const error = errorParser(response);
-        const errorObj = {
-          fullname: error?.fullname || '',
-          email: error?.email || '',
-          password: error?.password || '',
-          error: error?.error || false,
-        };
-        setErrorStatus(errorObj);
-      }
-      if (response.success) {
-        const errorObj = {
+  return (
+    <Formik
+      initialValues={
+        {
           fullname: '',
           email: '',
           password: '',
-          error: false,
-        };
-        setErrorStatus(errorObj);
-        setSuccessMsg(response.message.message);
+        }
       }
-    });
-  };
-  return (
-    <form
-      className="w-10/12 lg:w-4/12 border-2 border-gray-300 rounded flex flex-col p-2 lg:p-5"
+      validationSchema={toFormikValidationSchema(registerSchema)}
+      onSubmit={(values, actions) => {
+        signup({ ...values }).then((response: any) => {
+          actions.setSubmitting(false);
+          if (response.success && response.statusCode === StatusCodes.OK) {
+            actions.setStatus(response.message?.message);
+          } else {
+            actions.setStatus(response.message?.error);
+          }
+        });
+      }}
     >
-      <h2 className="text-xl font-semibold leading-7 text-gray-900 text-center mt-2">
-        Sign Up
-      </h2>
-      {successMsg && (
-        <h4 className="text-green-500 p-3 font-bold bg-green-50 my-2 rounded">
-          {successMsg}
-        </h4>
-      )}
-      <label htmlFor="fullname">Fullname</label>
-      <input
-        type="text"
-        name="fullname"
-        id="fullname"
-        aria-label="fullname"
-        className={`block flex-1 border-2 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 ${
-          errorStatus.error && errorStatus.fullname && 'border-red-500'
-        }`}
-        onChange={handleChange}
-        value={fields.fullname}
-      />
-      <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1 select-none">
-        {errorStatus.error && errorStatus.fullname}
-      </span>
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        aria-label="email"
-        value={fields.email}
-        onChange={handleChange}
-        className={`block flex-1 border-2 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 ${
-          errorStatus.error && errorStatus.email && 'border-red-500'
-        }`}
-      />
-      <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1 select-none">
-        {errorStatus.error && errorStatus.email}
-      </span>
+      {(form) => (
+        <Form
+          className="w-10/12 lg:w-4/12 border-2 border-gray-300 rounded flex flex-col p-2 lg:p-5"
+        >
+          <h2 className="text-xl font-semibold leading-7 text-gray-900 text-center mt-2">
+            Sign Up
+          </h2>
+          {form.status && (
+            <h4 className="text-green-500 p-3 font-bold bg-green-50 my-2 rounded">
+              {form.status}
+            </h4>
+          )}
+          <label htmlFor="fullname">Fullname</label>
+          <Field
+            type="text"
+            name="fullname"
+            id="fullname"
+            aria-label="fullname"
+            className={`block flex-1 border-2 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading${form.errors.fullname && form.touched.fullname && 'border-2 border-red-500'}`}
+          />
+          <ErrorMessage
+            name="fullname"
+            render={(msg) => (
+              <div className="text-red-500 pb-2">
+                {msg}
+              </div>
+            )}
+          />
+          <label htmlFor="email">Email</label>
+          <Field
+            type="email"
+            name="email"
+            id="email"
+            aria-label="email"
+            className={`block flex-1 border-2 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading${form.errors.email && form.touched.email && 'border-2 border-red-500'}`}
 
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        name="password"
-        id="password"
-        aria-label="password"
-        value={fields.password}
-        onChange={handleChange}
-        className={`block flex-1 border-2 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 ${
-          errorStatus.error && errorStatus.password && 'border-red-500'
-        }`}
-      />
-      <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1 select-none">
-        {errorStatus.error && errorStatus.password}
-      </span>
-      <button
-        type="submit"
-        onClick={onSubmit}
-        className="mt-4 mb-5 rounded bg-slate-600 px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        Sign Up
-      </button>
-    </form>
+          />
+          <ErrorMessage
+            name="email"
+            render={(msg) => (
+              <div className="text-red-500 pb-2">
+                {msg}
+              </div>
+            )}
+          />
+
+          <label htmlFor="password">Password</label>
+          <Field
+            type="password"
+            name="password"
+            id="password"
+            aria-label="password"
+            className={`block flex-1 border-2 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading${form.errors.password && form.touched.password && 'border-2 border-red-500'}`}
+
+          />
+          <ErrorMessage
+            name="password"
+            render={(msg) => (
+              <div className="text-red-500 pb-2">
+                {msg}
+              </div>
+            )}
+          />
+          {form.isSubmitting ? (
+            <button
+              type="button"
+              className="mt-4 mb-5 rounded bg-slate-600 px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              <Loading />
+              Please wait
+            </button>
+          ) : (
+            <button
+              type="submit"
+              className="mt-4 mb-5 rounded bg-slate-600 px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign Up
+            </button>
+          )}
+        </Form>
+      )}
+    </Formik>
   );
 }
 
--- src/components/auth/helpers/validationSchema.helper.ts
+++ src/components/auth/helpers/validationSchema.helper.ts
@@ -12,3 +12,36 @@ export const emailSchema = z.object({
 export const passwordSchema = z.object({
   password: z.string(),
 });
+
+export const registerSchema = z.object({
+  fullname: z.string().min(1).max(18).refine((value) => {
+    const parts = value.split(' ');
+    if (parts.length >= 2) {
+      // Check if all parts contain only alphabetic characters
+      // (allowing spaces or hyphens for compound names)
+      return parts
+        .every((part) => /^[a-zA-Z]+(?:-[a-zA-Z]+)?(?: [a-zA-Z]+(?:-[a-zA-Z]+)?)?$/.test(part));
+    }
+    return false;
+  }, {
+    message: 'Please provide both first and last names.',
+  }),
+  email: z.string().email().trim()
+    .min(1),
+  password: z.string().refine((value) => {
+    // Regular expressions for password validation criteria
+    const hasUpperCase = /[A-Z]/.test(value);
+    const hasLowerCase = /[a-z]/.test(value);
+    const hasNumber = /[0-9]/.test(value);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/.test(value);
+
+    return (
+      hasUpperCase
+        && hasLowerCase
+        && hasNumber
+        && hasSpecialChar
+    );
+  }, {
+    message: 'Try some strong password',
+  }),
+});
