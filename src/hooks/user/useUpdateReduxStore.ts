6ecc3b4cfa4acec2e23eb7018ae2e8628e141280
Hrishikesh Rajan <hrishikeshrajan3@gmail.com>
2023-12-04 14:13:59 +0530

    Chore: Local storage error handling modified

--- src/hooks/user/useUpdateReduxStore.ts
+++ src/hooks/user/useUpdateReduxStore.ts
@@ -1,15 +1,28 @@
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AuthHelper from '../../components/auth/apis/helper';
-import { addUser, confirmAuthentication } from '../../utils/reduxSlice/appSlice';
-import { useTypedDispatch } from './reduxHooks';
+import {
+  addUser, confirmAuthentication, removeAuthentication, removeUser,
+} from '../../utils/reduxSlice/appSlice';
+import { useTypedDispatch, useTypedSelector } from './reduxHooks';
 
 export const useUpdateReduxStore = () => {
   const dispatch = useTypedDispatch();
+  const navigate = useNavigate();
+  const isSignedOn = useTypedSelector((store) => store.app.authenticated);
   useEffect(() => {
     const data = AuthHelper.isSignedOn();
+
     if (data && Object.keys(data).length > 1) {
       dispatch(addUser(data));
       dispatch(confirmAuthentication(true));
+    }
+    if (!data && isSignedOn) {
+      AuthHelper.clearSignedOnData(() => {
+        dispatch(removeUser());
+        dispatch(removeAuthentication());
+        navigate('/auth');
+      });
     }
   }, []);
 };
