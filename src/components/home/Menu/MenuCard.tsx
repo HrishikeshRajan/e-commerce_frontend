0f90186c4443d8fa15e0a5afa01f6c00c1fc0799
Hrishikesh Rajan <hrishikeshrajan3@gmail.com>
2024-04-17 23:43:21 +0530

    Refactor: Category card and shimmer UI

--- src/components/home/Menu/MenuCard.tsx
+++ src/components/home/Menu/MenuCard.tsx
@@ -1,20 +1,20 @@
+/* eslint-disable react/no-unused-prop-types */
 /* eslint-disable react/require-default-props */
 import { Link } from 'react-router-dom';
 
-function MenuCard({ name, secure_url, offer = '' }:{ name:string, secure_url:string, offer?:string }) {
+function MenuCard({ name, secure_url }:{ name:string, secure_url:string, offer?:string }) {
   return (
 
-    <div className="relative w-28  lg:w-40 transition delay-50 ease-linear hover:scale-105 active:scale-105   border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className="relative w-36 h-72 lg:w-48 lg:h-96 transition delay-50 ease-linear hover:scale-105 active:scale-105   border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <Link to={{
         pathname: '/products',
         search: `?category=${name}`,
       }}
       >
-        <img className="w-full h-full " src={secure_url} alt="" />
+        <img className="w-full h-full object-cover" src={secure_url} alt="" />
 
-        <div className="bg-black text-center w-full absolute bottom-0 bg-opacity-80 ">
-          <p className="   mb-3 text-white font-extrabold">{name}</p>
-          {offer && <p className="  text-lg mb-3 text-white font-extrabold">{offer}</p>}
+        <div className="bg-black text-center items-center flex flex-col p-2 w-full absolute bottom-0 h-1/4 bg-opacity-80 ">
+          <p className=" text-white font-extrabold">{name}</p>
           <span className="text-white  mb-3 text-lg p-2">Shop Now</span>
 
         </div>
--- src/hooks/user/useCategories.ts
+++ src/hooks/user/useCategories.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { addCategories } from '@/utils/reduxSlice/productSlice';
 import { getCategories } from '@/components/home/api/getCategories';
 import { ErrorResponse, FetchApiResponse, hasFetchSucceeded } from '@/types/Fetch';
-import { useTypedDispatch } from './reduxHooks';
+import { useTypedDispatch, useTypedSelector } from './reduxHooks';
 
 /**
  * Fetches all categories
@@ -11,23 +11,25 @@ const useCategory = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string>();
   const dispatch = useTypedDispatch();
+  const categories = useTypedSelector((store) => store.products.categories);
   useEffect(() => {
     const abortController = new AbortController();
     const { signal } = abortController;
-
-    getCategories(signal)
-      .then((response: FetchApiResponse<{ categories:[] }> | ErrorResponse) => {
-        setLoading(false);
-        if (hasFetchSucceeded(response)) {
-          dispatch(addCategories(response.message.categories));
-        }
-      }).catch((err) => {
-        setLoading(false);
-        setError((err as Error).message);
-      });
+    if (categories && categories.length < 1) {
+      getCategories(signal)
+        .then((response: FetchApiResponse<{ categories:[] }> | ErrorResponse) => {
+          setLoading(false);
+          if (hasFetchSucceeded(response)) {
+            dispatch(addCategories(response.message.categories));
+          }
+        }).catch((err) => {
+          setLoading(false);
+          setError((err as Error).message);
+        });
+    }
 
     return () => abortController.abort();
-  }, [dispatch]);
+  }, [categories, dispatch]);
   return [loading, error] as const;
 };
 
