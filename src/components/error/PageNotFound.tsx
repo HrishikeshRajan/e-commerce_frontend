7c684f79f2e00e615cfeaafde7a4f72706f1b8f8
Hrishikesh Rajan <hrishikeshrajan3@gmail.com>
2024-04-23 15:22:47 +0530

    Router error page added ui changed

--- src/components/error/PageNotFound.tsx
+++ src/components/error/PageNotFound.tsx
@@ -2,8 +2,8 @@ import notFound from '../../assets/404.jpg';
 
 function PageNotFound() {
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-white text-center">
-      <img src={notFound} alt="not found" />
+    <div className="w-full h-screen flex justify-center items-center  bg-white text-center">
+      <img src={notFound} alt="not found" className="xl:w-96 xl:h-96 xl:rounded-full" />
     </div>
   );
 }
