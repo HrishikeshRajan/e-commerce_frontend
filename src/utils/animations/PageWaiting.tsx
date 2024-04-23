d681156bdfc1b54c1705061d070896088feae03f
Hrishikesh Rajan <hrishikeshrajan3@gmail.com>
2024-04-23 09:47:01 +0530

    Bug fix: auth loader ui

--- src/utils/animations/PageWaiting.tsx
+++ src/utils/animations/PageWaiting.tsx
@@ -3,19 +3,22 @@ import PuffLoader from 'react-spinners/PuffLoader';
 
 function PageWaiting({ loading }:{ loading:boolean }) {
   const override: CSSProperties = {
-    display: 'block',
+    display: 'flex',
     margin: '0 auto',
     borderColor: 'red',
   };
   return (
-    <div className="w-screen h-screen bg-cyan-500">
+    <div className="w-screen flex flex-col justify-center items-center h-screen bg-cyan-400">
+
       <PuffLoader
-        color="#FFFFFF"
+        color="white"
         loading={loading}
         cssOverride={override}
         aria-label="Loading Spinner"
+        size={100}
         data-testid="loader"
       />
+      <h1 className="text-white font-bold p-2">Please wait ...</h1>
     </div>
   );
 }
