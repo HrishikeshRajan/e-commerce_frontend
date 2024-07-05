81db7217bd1039b781609c2e85ba2d333c7b607d
Hrishikesh Rajan <hrishikeshrajan3@gmail.com>
2024-07-05 16:03:10 +0530

    bug fix: myOrder route auth protected

--- src/App.tsx
+++ src/App.tsx
@@ -125,7 +125,7 @@ const App = () => {
           path: 'myOrders',
           element: (
             <Suspense fallback={<PageWaiting loading />}>
-              <AuthenticationWrapper authentication={false}>
+              <AuthenticationWrapper authentication>
                 <Orders />
               </AuthenticationWrapper>
             </Suspense>
